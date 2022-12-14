c45bb01c2b6dea182bfd63a68f209e134fd2d6c3
Hayun Jung <60186469+Yun505@users.noreply.github.com>
2022-12-14 11:34:44 -0500

    fixed home_home is done

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,11 +6,10 @@ import backgroundImage from '../images/alumni1.jpg';
 export default function Home() {
   const { user, isLoading, isAuthenticated } = useAuth0();
     return(
-      <div style={{backgroundImage: `url(${backgroundImage}); background-size:cover`}}>
+      <div style={{backgroundImage: `url(${backgroundImage})`}}>
         <div className="container">
-          {/* <img src={backgroundImage} class="inline-block h-48 w-96 rounded-lg"/>  */}
           <div className="centered">
-            <h1 className="align-bottom text-5xl font-bold">Welcome to BCA Alumni{isAuthenticated ? ", " + user.name: null}!</h1>
+            <h1 className="title align-bottom text-5xl font-bold">Welcome to BCA Alumni{isAuthenticated ? ", " + user.name: null}!</h1>
             {!isAuthenticated ? (<LoginButton/>) :null}
           </div>
         </div>
