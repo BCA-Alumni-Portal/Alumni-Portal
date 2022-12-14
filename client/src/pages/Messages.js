cc4c14311f6320224e70894a0e50fc361f01c616
Hayun Jung <60186469+Yun505@users.noreply.github.com>
2022-12-14 15:46:13 -0500

    made basic messages screen outline

--- client/src/pages/Messages.js
+++ client/src/pages/Messages.js
@@ -1,3 +1,39 @@
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 export default function Messages() {
-    return <h1>Messages</h1>
-  }
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-5">
+          <h2>Contacts </h2>
+          <div className="list-group">
+            <a href="#" className="list-group-item list-group-item-action active" aria-current="true">
+              Hayun Jung
+            </a>
+            <a href="#" className="list-group-item list-group-item-action">Daniel Kim</a>
+            <a href="#" className="list-group-item list-group-item-action">Benen Sullivan</a>
+            <a href="#" className="list-group-item list-group-item-action">Remington Kim</a>
+            <a href="#" className="list-group-item list-group-item-action">Kevin Liu</a>
+          </div>
+        </div>
+        <div className="col-md-5">
+          <h2>Messages</h2>
+          <div className="message list-group">
+            <p className="other-message">Hi, my name's Hayun Jung</p>
+            <p className="self-message">Hello, nice to meet you. </p>
+            <p className="other-message">I was wondering if I could possibly intern at your office?</p>
+            <p className="self-message">I'll consider it.</p>
+          </div>
+          <div className="">
+            <Form.Group className="mb-3">
+              <Form.Control placeholder="Enter Text" />
+            </Form.Group>
+            <Button>Send</Button>
+            <Button>Upload File</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
