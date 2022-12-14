fefc55cc2b9594b1e7a3fa5203decb7ee8e09eef
liu-kevin1 <orgwirelist123@gmail.com>
2022-12-14 11:18:27 -0500

    jobs basic layout

--- client/src/pages/Jobs.js
+++ client/src/pages/Jobs.js
@@ -1,3 +1,31 @@
 export default function Jobs() {
-    return <h1>Jobs</h1>
-  }
+  return (
+
+    <div class="jobs">
+      
+      <h1>Jobs</h1>
+
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-5">
+            <h2>Job Postings</h2>
+            
+            <div class="list-group">
+              <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+                The current link item
+              </a>
+              <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+              <a href="#" class="list-group-item list-group-item-action">A third link item</a>
+              <a href="#" class="list-group-item list-group-item-action">A fourth link item</a>
+              <a class="list-group-item list-group-item-action disabled">A disabled link item</a>
+            </div>
+          </div>
+          <div class="col-md-7">
+            <h2 class="">Software Engineer</h2>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
+}
