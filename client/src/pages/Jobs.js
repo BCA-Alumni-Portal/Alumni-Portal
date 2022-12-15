0af9f545e86e92648a07349afc3ccfa6fd2193f0
orgwirelist123 <orgwirelist123@gmail.com>
2022-12-14 21:34:51 -0500

    Jobs Posting front end

--- client/src/pages/Jobs.js
+++ client/src/pages/Jobs.js
@@ -14,30 +14,65 @@ export default function Jobs() {
 
             <div class="list-group">
               <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
-                The current link item
+
+                <h3>
+                  <b>Software Engineer</b>
+                </h3>
+                <p>
+                  Company A USA Inc. Place, NJ (Hybrid)
+                  <br></br>
+                  Full-time
+                  <br></br>
+                  Posted by <b>Remington Kim</b>
+                </p>
+
               </a>
-              <a href="#" class="list-group-item list-group-item-action">A second link item</a>
-              <a href="#" class="list-group-item list-group-item-action">A third link item</a>
-              <a href="#" class="list-group-item list-group-item-action">A fourth link item</a>
-              <a class="list-group-item list-group-item-action disabled">A disabled link item</a>
+              <a href="#" class="list-group-item list-group-item-action">
+                
+              <h3>
+                  <b>Data Scientist</b>
+                </h3>
+                <p>
+                  Company B USA Inc. Place, NJ (Fully Remote)
+                  <br></br>
+                  Part-time
+                  <br></br>
+                  Posted by <b>Kevin Liu</b>
+                </p>
+
+              </a>
+              <a href="#" class="list-group-item list-group-item-action">
+                
+                <h3>
+                  <b>Software Tester</b>
+                </h3>
+                <p>
+                  Company C USA Inc. Place, NJ (In Person)
+                  <br></br>
+                  Full-time
+                  <br></br>
+                  Posted by <b>Hayun Jung</b>
+                </p>
+
+              </a>
             </div>
           </div>
           <div class="col-md-7">
             <div class="job-info">
               <h2 class="job-title">Software Engineer</h2>
-              <p class="job-location">Company USA Inc. Place, NJ (Hybrid)</p>
+              <p class="job-location">Company A USA Inc. Place, NJ (Hybrid)</p>
               <div class="job-schedule">
                 <img id="job-info-icon" src={briefcaseImage} width="25" height="25" alt="Test"></img>
                 <p>
                   Full-time
                 </p>
               </div>
-              
-              <div class="job-description">
 
+              <div class="job-description">
+                Employee will be working alongside other software engineers to engineer software. Background in software engineering recommended.
               </div>
 
-              <button id="job-apply">
+              <button class="job-apply">
                 Apply Now
               </button>
 
