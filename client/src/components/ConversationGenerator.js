c9a0965e9c726d14efb13d5442309ba9ca45ef6e
orgwirelist123 <orgwirelist123@gmail.com>
2023-02-12 21:05:27 -0500

    finally committing - can send messages - can receive messages - auto retrieve messages every 30 sec, or after sending a message - generate list of messages every update tick

---
+++ client/src/components/ConversationGenerator.js
@@ -0,0 +1,33 @@
+import { React, useState } from 'react'
+import data from "./ListData.json"
+
+
+function ConversationGenerator(props) {
+    //create a new array by filtering the original array
+    const filteredData = data.filter((el) => {
+        //if no input the return the original
+        if (props.input === '') {
+            return el;
+        }
+        //return the item which contains the user input
+        else {
+            return el.text.toLowerCase().includes(props.input)
+        }
+    })
+    return (
+        // <ul className="block border-black border">
+        // <li className="text-black  block  hover:bg-hover-cream py-3 text-sm" key={item.id}>{item.text}</li>
+
+        <div class="list-group">
+
+        {filteredData.map((item) => (
+            <li class="list-group-item list-group-item-action" key={item.id}>{item.text}</li>
+            ))}
+        </div>
+            
+        //</ul>
+    )
+}
+
+
+export default ConversationGenerator
