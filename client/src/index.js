470a284567b1cd9b83cef312a1173a5989271148
RemingtonKim <remingtonskim@gmail.com>
2023-02-26 23:03:02 -0500

    app css

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-import "./pages/styles/All.css"
+// import "./pages/styles/All.css"
+import "./App.css"
 import { BrowserRouter } from "react-router-dom"
 import { Auth0Provider } from "@auth0/auth0-react";
 
