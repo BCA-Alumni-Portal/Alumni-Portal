7d2b339f8b39515cd28841831aec072b1a51b6e3
RemingtonKim <remingtonskim@gmail.com>
2023-03-11 22:15:02 -0500

    new navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,6 +3,8 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { CgProfile } from 'react-icons/cg';
 import "./styles/NavBar.css";
 import logo from "../images/logo.png";
+import personImage2 from '../images/person2.png';
+
 
 export default function NavBar() {
   const { user, isLoading, isAuthenticated, loginWithRedirect, logout } = useAuth0();
@@ -11,32 +13,47 @@ export default function NavBar() {
       {!isAuthenticated ? (
         <div>
           <ul>
+            <Link to="/">
+              <img src={logo}></img>
+            </Link>
             <Link onClick={() => { loginWithRedirect({ redirectUri: 'http://localhost:3000' }) }}>
               <p className="nav-item text-2xl font-semibold border rounded py-2 px-2 hover:bg-gradient-to-r border-amber-50 hover:from-amber-400 hover:to-amber-500  border  rounded py-2 px-2  hover:border-amber-400 hover:text-white">Login</p>
             </Link>
           </ul>
         </div>
-        ) : (
-          <ul>
-            <CustomLink to="/me">
-              <p className="nav-item text-2xl font-semibold border rounded py-2 px-2 hover:bg-gradient-to-r hover:from-amber-400 hover:to-amber-500  border border-amber-50 rounded py-2 px-2  hover:border-amber-400 hover:text-white">Me</p>
-            </CustomLink>  
-            <CustomLink to="/people">
-              <p className="nav-item text-2xl font-semibold border rounded py-2 px-2 hover:bg-gradient-to-r hover:from-amber-400 hover:to-amber-500  border border-amber-50 rounded py-2 px-2  hover:border-amber-400 hover:text-white">People</p>
-            </CustomLink>
-            <CustomLink to="/messages">
-              <p className="nav-item text-2xl font-semibold border rounded py-2 px-2 hover:bg-gradient-to-r hover:from-amber-400 hover:to-amber-500  border border-amber-50 rounded py-2 px-2  hover:border-amber-400 hover:text-white">Messages</p>
-            </CustomLink>
-            <CustomLink to="/admin">
-              <p className="nav-item text-2xl font-semibold border rounded py-2 px-2 hover:bg-gradient-to-r hover:from-amber-400 hover:to-amber-500  border border-amber-50 rounded py-2 px-2  hover:border-amber-400 hover:text-white">Admin</p>
-            </CustomLink>
-          <Link onClick={() => logout({ returnTo: "http://localhost:3000" })}>
-            <p className="nav-item text-2xl font-semibold border rounded py-2 px-2 hover:bg-gradient-to-r hover:from-amber-400 hover:to-amber-500  border border-amber-50 rounded py-2 px-2  hover:border-amber-400 hover:text-white">Logout</p>
+      ) : (
+        <ul>
+          <Link to="/">
+            <img src={logo}></img>
           </Link>
+
+          <CustomLink to="/people">
+            <p className="nav-item text-2xl font-semibold border rounded py-2 px-2 hover:bg-gradient-to-r hover:from-amber-400 hover:to-amber-500  border border-amber-50 rounded py-2 px-2  hover:border-amber-400 hover:text-white">People</p>
+          </CustomLink>
+          <CustomLink to="/messages">
+            <p className="nav-item text-2xl font-semibold border rounded py-2 px-2 hover:bg-gradient-to-r hover:from-amber-400 hover:to-amber-500  border border-amber-50 rounded py-2 px-2  hover:border-amber-400 hover:text-white">Messages</p>
+          </CustomLink>
+          <CustomLink to="/admin">
+            <p className="nav-item text-2xl font-semibold border rounded py-2 px-2 hover:bg-gradient-to-r hover:from-amber-400 hover:to-amber-500  border border-amber-50 rounded py-2 px-2  hover:border-amber-400 hover:text-white">Admin</p>
+          </CustomLink>
         </ul>)}
-      <Link to="/">
-        <img src={logo}></img>
-      </Link>
+      <div className="dropdown dropdown-hover dropdown-end">
+        <Link to='/me'>
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img src={personImage2} />
+            </div>
+          </label>
+        </Link>
+        <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-40 mt-16">
+          {/* <CustomLink className="nav-item text-xl font-semibold border rounded hover:bg-gradient-to-r hover:from-amber-400 hover:to-amber-500  border border-amber-50 rounded py-2 px-2  hover:border-amber-400 hover:text-white" to="/me">
+            Me
+          </CustomLink> */}
+          <CustomLink className="nav-item text-lg font-semibold border rounded hover:bg-gradient-to-r hover:from-amber-400 hover:to-amber-500  border border-amber-50 rounded py-2 px-2  hover:border-amber-400 hover:text-white" onClick={() => logout({ returnTo: "http://localhost:3000" })}>
+            Logout
+          </CustomLink>
+        </ul>
+      </div>
     </nav>
   )
 }
