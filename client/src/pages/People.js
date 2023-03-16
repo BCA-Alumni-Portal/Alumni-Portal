b922ff723866046aa586fe9f594dc1d4b4f62454
Kevin Liu <kevliu.dev@gmail.com>
2023-03-16 12:15:01 -0400

    fixed academy filter rendering

--- client/src/pages/People.js
+++ client/src/pages/People.js
@@ -9,6 +9,7 @@ import PeopleGeneratorComponent from "../components/PeopleGenerator";
 import Person from '../components/Person';
 import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
+import './styles/People.css'
 import Home from './Home';
 
 function useInterval(callback, delay) {
@@ -180,7 +181,7 @@ function People() {
     // console.log("interval called");
     submitGetPeopleRequest();
     requestClientID();
-  }, 1000);
+  }, 5000);
 
   if (auth) {
     return (
@@ -223,7 +224,7 @@ function People() {
                 <ul className="block menu menu-horizontal bg-base-100 w-30 align-self-left">
                   <li tabIndex="0">
                     <span className="text-sm border border-1 border-red-300">Academy</span>
-                    <ul className="menu bg-base-100 w-30 border border-1 border-red-300">
+                    <ul className="people-list menu bg-base-100 w-30 border border-1 border-red-300">
                       <li className="hover:bg-stone-200 focus:none">
                         <div className="hover:bg-stone-200 text-black hover:border-stone-100">
                           <label className="flex" >
@@ -302,6 +303,7 @@ function People() {
             <br className="space-y-5"></br>
             <YearFilterComponent input={YearFilter}></YearFilterComponent>
             <AcademyFilterComponent input={AcademyFilter}></AcademyFilterComponent>
+            {/* <br className="space-y-5"></br>
             <br className="space-y-5"></br>
             <br className="space-y-5"></br>
             <br className="space-y-5"></br>
@@ -315,8 +317,7 @@ function People() {
             <br className="space-y-5"></br>
             <br className="space-y-5"></br>
             <br className="space-y-5"></br>
-            <br className="space-y-5"></br>
-            <br className="space-y-5"></br>
+            <br className="space-y-5"></br> */}
             <br className="space-y-5"></br>
             <br className="space-y-5"></br>
             <br className="space-y-5"></br>
