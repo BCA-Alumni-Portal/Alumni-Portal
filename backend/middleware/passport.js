7366631a20d6fa222841382a750e93ea386273b3
RemingtonKim <remingtonskim@gmail.com>
2023-03-17 13:56:26 -0400

    new url

--- backend/middleware/passport.js
+++ backend/middleware/passport.js
@@ -8,7 +8,7 @@ const auth0Strategy = new Auth0Strategy(
         domain: process.env.AUTH0_DOMAIN,
         clientID: process.env.AUTH0_CLIENT_ID,
         clientSecret: process.env.AUTH0_SECRET,
-        callbackURL: process.env.AUTH0_CALLBACK_URL,
+        callbackURL: (process.env.NODE_ENV=="development" ? 'http://localhost:3000/': "https://academiesalumni-production.up.railway.app/") + process.env.AUTH0_CALLBACK_URL,
     },
     (accessToken, refreshToken, extraParams, profile, done) => {
         return done(null, profile);
